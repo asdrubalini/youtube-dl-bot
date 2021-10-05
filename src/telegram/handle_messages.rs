ab74d351b18336634ebdfc18b78b7f9db34df150
Asdrubalini <indirizzoemail@mail.com>
2021-10-06 00:21:53 +0200

    typo

--- src/telegram/handle_messages.rs
+++ src/telegram/handle_messages.rs
@@ -6,7 +6,7 @@ pub async fn handle_messages(api: &Api) {
     while let Some(update) = stream.next().await {
         let update = update.unwrap();
 
-        // Only match on messages that starts with "/download"
+        // Only match messages that starts with "/download"
         match update.kind {
             telegram_bot::UpdateKind::Message(message) => match message.kind {
                 telegram_bot::MessageKind::Text {
